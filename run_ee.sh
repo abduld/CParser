41ceee1b26499f4a22d6566f9f070b099750a6ac
Abdul Dakkak <dakkak@illinois.edu>
2016-04-17 18:18:24 -0500

    update

--- run_ee.sh
+++ run_ee.sh
@@ -1,1 +1,10 @@
-./lcdecl -Zpass -D__has_builtin\(x\)=1 -D__has_feature\(x\)=1  -D__x86_64__=1  -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS  -std=c11 ~/Code/llvm/include/llvm-c/ExecutionEngine.h -o ExecutionEngine.json
+#!/bin/sh
+
+LLVM_DIR=$HOME/Code/llvm/include/llvm-c
+
+rm -fr *.json
+
+for ii in ${LLVM_DIR}/*h; do
+    echo `basename ${ii}`
+    ./lcdecl -Zpass -D__has_builtin\(x\)=1 -D__has_feature\(x\)=1  -D__x86_64__=1  -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS  -std=c11 "${ii}" -o `basename ${ii} .h`.json;
+done
